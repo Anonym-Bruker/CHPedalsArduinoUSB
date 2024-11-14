e13e2b7089a8bfbd489ed3196a94a9e9e3d03623
unknown <github@modalsli.com>
2024-11-14 14:39:57 +0100

    Added pictures

--- Simple_Gameport_Joystick.ino
+++ Simple_Gameport_Joystick.ino
@@ -1,8 +1,18 @@
 
-/*  Script for converting CH Pedals to USB
- *  You might consider commenting out the Serial.print lines when not debugging 
- * 
- *  NB!: Created for Arduino Leonardo VID: 2341 PID: 8036
+/*            Throttle script with filtering
+ *  This script takes three axis, and filters the signal for an output 
+ *  at 50 Hz. The clock is used to exactly time the polling, 
+ *  to avoid sampling bias. A crude bandpass filter throws 
+ *  away spikes that are impossible(!) to result from user 
+ *  input. This will convert spikes on the control axis into 
+ *  a mere stutter. Then a Kalman filter is applied to the 
+ *  20 Hz signal to remove remaining gaussian noise. 
+ *  All axes each get their own one dimensional Kalman 
+ *  because there is no correlation that is relevant for the 
+ *  filter. The constants in this script are configured for 
+ *  the Saitek Pro throttle quadrant.
+ *  
+ *  BN: Arduino Leonardo VID: 2341 PID: 8036
 */
 
 #include "Joystick.h"
